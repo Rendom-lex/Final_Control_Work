690cdd16446589da165b98c0116e252db79d3ba7
Rendom-lex <140806354+Rendom-lex@users.noreply.github.com>
2023-10-17 14:41:58 +0300

    Done Step 4
    Выполнил пункт 4 КР. Написал код для решения задачи. Пункты 2, 3 и 4 загружены в репозиторий отдельными коммитами.

---
+++ Step 3/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> result = new List<string>();
+        List<string> inputArray = new List<string> 
+        {
+            "4856",
+            "banana",
+            "21",
+            "7",
+            "elephant",
+            "fox"
+        };
+
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                result.Add(item);
+            }
+        }
+
+        foreach (string item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
